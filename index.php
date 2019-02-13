ade47e4e18bd23528d90c90db7d952843da19c1b
SergejShe1 <46597210+SergejShe1@users.noreply.github.com>
2019-02-13 23:12:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Chess Online</title>#
+
+</head>
+<body>
+
+<?php
+
+$board = createBoard(8, '0');
+renderBoard($board);
+$board = setValue($board, 5, 5, 'k');
+renderBoard($board);
+
+function setValue(array $board, int $i, int $a, string $value): array
+{
+    $board[$i][$a] = $value;
+
+    return $board;
+}
+
+
+function createBoard(int $n, string $value): array
+{
+    $n++;
+    $board = [];
+    for ($i = 1; $i < $n; $i++) {
+        for ($a = 1; $a < $n; $a++) {
+            $board[$i][$a] = $value;
+        }
+    }
+
+    return $board;
+}
+
+function renderBoard(array $board): void
+{
+    echo '----------------BOARD----------------</br>';
+    foreach ($board as $row) {
+        echo '<br>';
+        foreach ($row as $item) {
+            echo $item.' ';
+        }
+    }
+}
+
+?>
+</body>
+
+</html>
